7dea44150931551613a45fd6fcd22c457443b653
Théo Melly <theomelly@gmail.com>
2025-04-11 18:43:21 +0200

    Theo - modif de la vue

--- Project files/src/controleur/ShoppingController.java
+++ Project files/src/controleur/ShoppingController.java
@@ -1,9 +1,7 @@
 package controleur;
-import javax.swing.JFrame;
-import vue.ShoppingView;
+
 import modele.ShoppingModel;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import vue.ShoppingView;
 
 public class ShoppingController {
     private ShoppingModel model;
@@ -13,36 +11,5 @@ public class ShoppingController {
         this.model = model;
         this.view = view;
 
-        view.getToggleButton().addActionListener(new ActionListener() {
-            private boolean isFullscreen = true;
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (isFullscreen) {
-                    view.getFrame().setExtendedState(JFrame.NORMAL);
-                    view.getFrame().setSize(800, 600);
-                    view.getToggleButton().setText("Mettre en plein écran");
-                } else {
-                    view.getFrame().setExtendedState(JFrame.MAXIMIZED_BOTH);
-                    view.getToggleButton().setText("Enlever l'écran plein");
-                }
-                isFullscreen = !isFullscreen;
-            }
-        });
-
-        view.getLoginButton().addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                view.showPage("Login");
-            }
-        });
-
-        view.getRegisterButton().addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                view.showPage("Register");
-            }
-        });
     }
 }
