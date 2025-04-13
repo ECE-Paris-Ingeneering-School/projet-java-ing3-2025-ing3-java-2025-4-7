282d1f3b800096f4d07ad3fcf98109341bd4381a
NicoDuzan <nicolas.duzan@edu.ece.fr>
2025-04-13 16:37:46 +0200

    Modif de la sql

--- Project files/BDD/BDD.sql
+++ Project files/BDD/BDD.sql
@@ -31,16 +31,24 @@ SET time_zone = "+00:00";
 -- Structure de la table `clients`
 --
 
-DROP TABLE IF EXISTS `clients`;
-CREATE TABLE IF NOT EXISTS `clients` (
-  `clientID` int NOT NULL AUTO_INCREMENT,
-  `clientPrenom` varchar(20) NOT NULL,
-  `clientNom` varchar(20) NOT NULL,
-  `clientMail` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `clientMDP` varchar(12) NOT NULL,
-  PRIMARY KEY (`clientID`)
+DROP TABLE IF EXISTS `utilisateur`;
+CREATE TABLE IF NOT EXISTS `utilisateur` (
+  `utilisateurID` int NOT NULL AUTO_INCREMENT,
+  `utilisateurPrenom` varchar(50) NOT NULL,
+  `utilisateurNom` varchar(50) NOT NULL,
+  `utilisateurMail` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL UNIQUE,
+  `utilisateurMDP` varchar(100) NOT NULL,
+  PRIMARY KEY (`utilisateurID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+DROP TABLE IF EXISTS `client`;
+CREATE TABLE IF NOT EXISTS `client` (
+    `clientID` int NOT NULL AUTO_INCREMENT,
+    `clientAdresse` varchar(50) NOT NULL,
+    `clientTel` varchar(50) NOT NULL,
+    FOREIGN KEY (clientID) REFERENCES utilisateur(utilisateurID) ON DELETE CASCADE,
+    PRIMARY KEY (`clientID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Structure de la table `articles`
@@ -49,11 +57,11 @@ CREATE TABLE IF NOT EXISTS `clients` (
 DROP TABLE IF EXISTS `articles`;
 CREATE TABLE IF NOT EXISTS `articles` (
   `articleID` int NOT NULL,
-  `articleNom` varchar(20) NOT NULL,
-  `articleMarque` varchar(20) NOT NULL,
-  `articlePrix_unitaire` double NOT NULL,
-  `articlePrix_vrac` double NOT NULL,
-  `articleSeuil_vrac` int NOT NULL,
+  `articleNom` varchar(50) NOT NULL,
+  `articleMarque` varchar(50) NOT NULL,
+  `articlePrix_unitaire` FLOAT NOT NULL,
+  `articlePrix_vrac` FLOAT NOT NULL,
+  `articleSeuil_vrac` FLOAT NOT NULL,
   `articleStock` int NOT NULL,
   PRIMARY KEY (`articleID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -66,10 +74,7 @@ CREATE TABLE IF NOT EXISTS `articles` (
 DROP TABLE IF EXISTS `admin`;
 CREATE TABLE IF NOT EXISTS `admin` (
    `adminID` int NOT NULL AUTO_INCREMENT,
-   `adminPrenom` varchar(20) NOT NULL,
-   `adminNom` varchar(20) NOT NULL,
-   `adminMail` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-   `adminMDP` varchar(12) NOT NULL,
+   FOREIGN KEY (adminID) REFERENCES utilisateur(utilisateurID) ON DELETE CASCADE,
    PRIMARY KEY (`adminID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -81,10 +86,11 @@ CREATE TABLE IF NOT EXISTS `admin` (
 DROP TABLE IF EXISTS `commande_totale`;
 CREATE TABLE IF NOT EXISTS `commande_totale` (
    `commandeID` int NOT NULL AUTO_INCREMENT,
-   `adminPrenom` varchar(20) NOT NULL,
-   `commandeDate` varchar(20) NOT NULL,
-   `commandeCout_total` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-   `adminMDP` varchar(12) NOT NULL,
+   `clientID` int NOT NULL,
+   `commandeDate` DATE NOT NULL,
+   `statut_commande`varchar(20) NOT NULL,
+   `commande_total` FLOAT,
+   FOREIGN KEY (clientID) REFERENCES client(clientID) ON DELETE SET NULL,
    PRIMARY KEY (`commandeID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
