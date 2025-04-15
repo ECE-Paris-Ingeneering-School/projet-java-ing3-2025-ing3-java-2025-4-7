c85d0a3f274478955adc51606f11d2279ddc8e2d
jade <jade.parrilla@edu.ece.fr>
2025-04-15 16:45:49 +0200

    15/04/2025 - Modification modèle commande

--- Project files/src/modele/Commande.java
+++ Project files/src/modele/Commande.java
@@ -3,16 +3,20 @@ package modele;
 public class Commande {
     private final int id;
     private final int clientID;
-    private String date;
+    private final String date;
     private String statut;
-    private double prix;
+    private final double prix;
+    private final String listeID_Article;
+    private final String listeQuantite_Article;
 
-    public Commande(int id, int clientID, String date, String statut, double prix) {
+    public Commande(int id, int clientID, String date, String statut, double prix, String listeID_Article, String listeQuantite_Article) {
         this.id = id;
         this.clientID = clientID;
         this.date = date;
         this.statut = statut;
         this.prix = prix;
+        this.listeID_Article = listeID_Article;
+        this.listeQuantite_Article = listeQuantite_Article;
     }
 
     public int getId() { return id; }
@@ -25,10 +29,10 @@ public class Commande {
 
     public double getPrix() { return prix; }
 
-    public void setDate(String date) { this.date = date; }
+    public String getListeID_Article() { return listeID_Article; }
+
+    public String getListeQuantite_Article() { return listeQuantite_Article; }
 
     public void setStatut(String statut) { this.statut = statut; }
-
-    public void setPrix(double prix) { this.prix = prix; }
 }
 
